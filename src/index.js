dcadcf61f85f88bff6cb85fd8a142a1e3530bcd8
Marina Miroslava <argenty@gmail.com>
2021-08-19 17:42:05 +0300

    everything works

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
+exports.min = function min(array) {
+    if (array == null || array == undefined || array == NaN) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
+    if (array.length == 0) {
+        return 0;
+    }
+
+    let x = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (x > array[i]) {
+            x = array[i];
+        }
+    }
+    return x;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array == null || array == undefined || array == NaN) {
+        return 0;
+    }
+
+    if (array.length == 0) {
+        return 0;
+    }
+
+    let x = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (x < array[i]) {
+            x = array[i];
+        }
+    }
+    return x;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array == null || array == undefined || array == NaN || array.length == 0) {
+        return 0;
+    }
+
+    let x = 0;
+    for (let i = 0; i < array.length; i++) {
+        x = x + array[i];
+    }
+
+    return x / array.length;
+
+
 }
